816e88490b5ce69a16bd0c8547629b6aabbb2f26
MoussaNdaodev <159473834+MoussaNdaodev@users.noreply.github.com>
2026-02-01 18:32:58 +0000

    Refactor CouponSeeder to use updateOrInsert method

--- database/seeders/CouponSeeder.php
+++ database/seeders/CouponSeeder.php
@@ -1,32 +1,34 @@
 <?php
+
 namespace Database\Seeders;
+
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
 class CouponSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
     public function run()
     {
-        $data=array(
-            array(
-                'code'=>'senMecanique',
-                'type'=>'fixed',
-                'value'=>'300',
-                'status'=>'active'
-            ),
-            array(
-                'code'=>'senMecanique2',
-                'type'=>'percent',
-                'value'=>'10',
-                'status'=>'active'
-            ),
-        );
+        $coupons = [
+            [
+                'code' => 'senMecanique',
+                'type' => 'fixed',
+                'value' => 300,
+                'status' => 'active',
+            ],
+            [
+                'code' => 'senMecanique2',
+                'type' => 'percent',
+                'value' => 10,
+                'status' => 'active',
+            ],
+        ];
 
-        DB::table('coupons')->insert($data);
+        foreach ($coupons as $coupon) {
+            DB::table('coupons')->updateOrInsert(
+                ['code' => $coupon['code']],
+                $coupon
+            );
+        }
     }
 }
