c808e04824bb6b3b9c788c60dce67bb9bcec8d70
MoussaNdaodev <159473834+MoussaNdaodev@users.noreply.github.com>
2026-02-01 12:39:00 +0000

    Replace asset() with mix() for asset loading

--- resources/views/frontend/layouts/head.blade.php
+++ resources/views/frontend/layouts/head.blade.php
@@ -1,80 +1,87 @@
 <!-- Meta Tag -->
 @yield('meta')
+
 <!-- Title Tag  -->
 <title>@yield('title')</title>
+
 <!-- Favicon -->
-<link rel="icon" type="image/png" href="{{asset('frontend/img/favicon.png')}}">
+<link rel="icon" type="image/png" href="{{ mix('frontend/img/favicon.png') }}">
+
 <!-- Web Font -->
 <link href="https://fonts.googleapis.com/css?family=Poppins:200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i&display=swap" rel="stylesheet">
 
 <!-- StyleSheet -->
 <link rel="manifest" href="/manifest.json">
+
 <!-- Bootstrap -->
-<link rel="stylesheet" href="{{asset('frontend/css/bootstrap.css')}}">
+<link rel="stylesheet" href="{{ mix('frontend/css/bootstrap.css') }}">
+
 <!-- Magnific Popup -->
-<link rel="stylesheet" href="{{asset('frontend/css/magnific-popup.min.css')}}">
+<link rel="stylesheet" href="{{ mix('frontend/css/magnific-popup.min.css') }}">
+
 <!-- Font Awesome -->
-<link rel="stylesheet" href="{{asset('frontend/css/font-awesome.css')}}">
+<link rel="stylesheet" href="{{ mix('frontend/css/font-awesome.css') }}">
+
 <!-- Fancybox -->
-<link rel="stylesheet" href="{{asset('frontend/css/jquery.fancybox.min.css')}}">
+<link rel="stylesheet" href="{{ mix('frontend/css/jquery.fancybox.min.css') }}">
+
 <!-- Themify Icons -->
-<link rel="stylesheet" href="{{asset('frontend/css/themify-icons.css')}}">
+<link rel="stylesheet" href="{{ mix('frontend/css/themify-icons.css') }}">
+
 <!-- Nice Select CSS -->
-<link rel="stylesheet" href="{{asset('frontend/css/niceselect.css')}}">
+<link rel="stylesheet" href="{{ mix('frontend/css/niceselect.css') }}">
+
 <!-- Animate CSS -->
-<link rel="stylesheet" href="{{asset('frontend/css/animate.css')}}">
+<link rel="stylesheet" href="{{ mix('frontend/css/animate.css') }}">
+
 <!-- Flex Slider CSS -->
-<link rel="stylesheet" href="{{asset('frontend/css/flex-slider.min.css')}}">
+<link rel="stylesheet" href="{{ mix('frontend/css/flex-slider.min.css') }}">
+
 <!-- Owl Carousel -->
-<link rel="stylesheet" href="{{asset('frontend/css/owl-carousel.css')}}">
+<link rel="stylesheet" href="{{ mix('frontend/css/owl-carousel.css') }}">
+
 <!-- Slicknav -->
-<link rel="stylesheet" href="{{asset('frontend/css/slicknav.min.css')}}">
+<link rel="stylesheet" href="{{ mix('frontend/css/slicknav.min.css') }}">
+
 <!-- Jquery Ui -->
-<link rel="stylesheet" href="{{asset('frontend/css/jquery-ui.css')}}">
+<link rel="stylesheet" href="{{ mix('frontend/css/jquery-ui.css') }}">
+
+<!-- Icons & Fonts (CDN – inchangés) -->
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 
-<meta name="csrf-token" content="{{csrf_token()}}">
+<meta name="csrf-token" content="{{ csrf_token() }}">
 
- {{-- <!-- Inclure Leaflet -->
-    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css" />
-    <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"></script>
-
-    <!-- Inclure Leaflet Routing Machine -->
-    <script src="https://unpkg.com/leaflet-routing-machine@latest/dist/leaflet-routing-machine.js"></script> --}}
-
-    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.2.0/dist/leaflet.css" />
-    <link rel="stylesheet" href="https://unpkg.com/leaflet-routing-machine@latest/dist/leaflet-routing-machine.css" />
-    <script src="https://unpkg.com/leaflet@1.2.0/dist/leaflet.js"></script>
-    <script src="https://unpkg.com/leaflet-routing-machine@latest/dist/leaflet-routing-machine.js"></script>
+<!-- Leaflet -->
+<link rel="stylesheet" href="https://unpkg.com/leaflet@1.2.0/dist/leaflet.css" />
+<link rel="stylesheet" href="https://unpkg.com/leaflet-routing-machine@latest/dist/leaflet-routing-machine.css" />
+<script src="https://unpkg.com/leaflet@1.2.0/dist/leaflet.js"></script>
+<script src="https://unpkg.com/leaflet-routing-machine@latest/dist/leaflet-routing-machine.js"></script>
 
 <!-- Eshop StyleSheet -->
-<link rel="stylesheet" href="{{asset('frontend/css/reset.css')}}">
-<link rel="stylesheet" href="{{asset('frontend/css/style.css')}}">
-<link rel="stylesheet" href="{{asset('frontend/css/responsive.css')}}">
-{{-- <script type='text/javascript' src='https://platform-api.sharethis.com/js/sharethis.js#property=5f2e5abf393162001291e431&product=inline-share-buttons' async='async'></script> --}}
-{{-- <script type="text/javascript" src="https://platform-api.sharethis.com/js/sharethis.js#property=66af75773ca64b0019342ebf&product=image-share-buttons&source=platform" async="async"></script> --}}
+<link rel="stylesheet" href="{{ mix('frontend/css/reset.css') }}">
+<link rel="stylesheet" href="{{ mix('frontend/css/style.css') }}">
+<link rel="stylesheet" href="{{ mix('frontend/css/responsive.css') }}">
+
 <style>
     /* Multilevel dropdown */
     .dropdown-submenu {
-    position: relative;
+        position: relative;
     }
 
     .dropdown-submenu>a:after {
-    content: "\f0da";
-    float: right;
-    border: none;
-    font-family: 'FontAwesome';
+        content: "\f0da";
+        float: right;
+        border: none;
+        font-family: 'FontAwesome';
     }
 
     .dropdown-submenu>.dropdown-menu {
-    top: 0;
-    left: 100%;
-    margin-top: 0px;
-    margin-left: 0px;
+        top: 0;
+        left: 100%;
+        margin-top: 0px;
+        margin-left: 0px;
     }
-
-    /*
 </style>
+
 @stack('styles')
